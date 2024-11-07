97305100fca226f486d6c4930792669d79890f64
Duck-Citizenf <slavabogdanov98@gmail.com>
2024-11-07 20:57:38 +0300

    a

--- js.js
+++ js.js
@@ -1,4 +1,13 @@
-document.querySelector(".arrow-active").addEventListener('click', move);
-function move(){
-    document.getElementById('gallery')
+document.getElementById("second-arrow").addEventListener('click', moveforward);
+document.getElementById("first-arrow").addEventListener('click', movebackward);
+
+function moveforward(){
+    document.getElementById("first-arrow").classList="arrow-button arrow-active";
+    document.getElementById("second-arrow").classList="arrow-button arrow-block";
+    document.getElementById('gallery').classList = "flex gallery-2";
+}
+function movebackward(){
+    document.getElementById("second-arrow").classList="arrow-button arrow-active";
+    document.getElementById("first-arrow").classList="arrow-button arrow-block";
+    document.getElementById('gallery').classList = "flex gallery";
 }
