aee7a3c4331bfb7447bbc6a3dc6668789b4eb824
Duck <bogdanovvv@elmeh.ru>
2024-11-08 15:20:32 +0300

    a

--- js.js
+++ js.js
@@ -5,9 +5,11 @@ function moveforward(){
     document.getElementById("first-arrow").classList="arrow-button arrow-active";
     document.getElementById("second-arrow").classList="arrow-button arrow-block";
     document.getElementById('gallery').classList = "flex gallery-2";
+    document.getElementById('counter').innerText = "6 / 6";
 }
 function movebackward(){
     document.getElementById("second-arrow").classList="arrow-button arrow-active";
     document.getElementById("first-arrow").classList="arrow-button arrow-block";
     document.getElementById('gallery').classList = "flex gallery";
+    document.getElementById('counter').innerText = "3 / 6";
 }
